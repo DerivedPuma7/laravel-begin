7d6bc3bf6cf1bcfb6c171f0cc4ffe794ff27fa73
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2022-06-27 18:37:37 -0300

    feat: rotas do CRUD de produtos da api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function(Request $request) {
+    // dd($request->headers->get('auth'));
+    // dd($request->headers->all());
+
+    $response = new \Illuminate\Http\Response(json_encode(['message' => 'minha primeira resposta']));
+    $response->header('Content-Type', 'application/json');
+    return $response;
+});
+
+Route::namespace('App\Http\Controllers\Api')->group(function() {
+
+    //Products Routes
+    Route::prefix('products')->group(function() {
+        Route::get('/', 'ProductController@index');
+        Route::get('/{id}', 'ProductController@show');
+        Route::post('/', 'ProductController@save');
+        Route::put('/', 'ProductController@update');
+        Route::patch('/', 'ProductController@update');
+        Route::delete('/{id}', 'ProductController@delete');
+    });
+
+    Route::resource('/users', 'UserController');
+});
